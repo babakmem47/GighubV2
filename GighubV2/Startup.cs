a6f03ca16ce43f798db862367e56be4bc347e123
babak35102 <babak35102@gmail.com>
2018-12-26 08:55:40 +0330

    Raw MVC Project

---
+++ GighubV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GighubV2.Startup))]
+namespace GighubV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
