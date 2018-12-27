f456d5673b3a7283b5d1eaa0975bccc2c11cb938
bk_Home <mohebbi@ebanksepah.ir>
2018-12-27 14:12:05 +0330

    Add GigFormViewModel.cs + Date and Time TextBox in Create.chtml

---
+++ GighubV2/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GighubV2.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+    }
+}
