cc8c6ac9678b7a1136600cbe5efb4ec5bf24f624
bk_Home <mohebbi@ebanksepah.ir>
2018-12-27 10:42:12 +0330

    Override Conventions for Gig.cs and Genre.cs

--- GighubV2/Models/Genre.cs
+++ GighubV2/Models/Genre.cs
@@ -1,9 +1,13 @@
-﻿namespace GighubV2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GighubV2.Models
 {
     public class Genre
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
     }
--- GighubV2/Models/Gig.cs
+++ GighubV2/Models/Gig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GighubV2.Models
 {
@@ -6,12 +7,16 @@ namespace GighubV2.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public ApplicationUser Artist { get; set; }
 
         public DateTime DateTime { get; set; }
 
+        [Required]
+        [StringLength(200)]
         public string Venue { get; set; }
 
+        [Required]
         public Genre Genre { get; set; }
 
     }
