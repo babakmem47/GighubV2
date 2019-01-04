29a81357f9ecbfc9e79abd2f62f2ce5307b3ea59
bk_Home <mohebbi@ebanksepah.ir>
2019-01-05 00:11:29 +0330

    Create WebApi AttendancesController to get a gigId and create an attendance and save it to Attendances table. Not testing it yet

---
+++ GighubV2/Controllers/AttendancesController.cs
@@ -0,0 +1,35 @@
+﻿using GighubV2.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+
+namespace GighubV2.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(int gigId)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                //                Gig = _context.Gigs.Single(g => g.Id == gigId),
+                AttendeeId = currentUserId
+                //                Attendee = _context.Attendances.Single(a => a.AttendeeId == currentUserId)
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
