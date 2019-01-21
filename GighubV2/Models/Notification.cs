49f14cc2aff3d116758e2f1cc12063ad2dd12e5c
babak35102 <babak35102@gmail.com>
2019-01-21 16:25:43 +0330

    Add NotificationType Enum

---
+++ GighubV2/Models/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GighubV2.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public DateTime OriginalDateTime { get; set; }
+
+        public string OriginalValue { get; set; }
+
+    }
+}
